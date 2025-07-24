422ed39623737e6c79cccf00fc283fba3958f34b
trendblockx <mankind0002@gmail.com>
2025-07-24 08:30:41 +0530

    test

---
+++ Javascript- Basics/Variables.js
@@ -0,0 +1,14 @@
+const accountId = 13242342
+let accountEmail = "mannjinder@google.com"
+var accountPassword = "12113"
+accountCity = "Amritsar"
+let accountState;
+
+// accountId = 3
+accountEmail = "rusty@gmail.com"
+accountPassword = "0934"
+accountCity = "mehta"
+
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
